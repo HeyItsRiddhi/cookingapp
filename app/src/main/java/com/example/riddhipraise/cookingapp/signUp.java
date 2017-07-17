8845c65cbcf1115be0545a0fa472ff31cc05ae65
PRAISEAyorinde <praise.ayorinde95@gmail.com>
2017-07-17 07:38:43 -0400

    signup, and mini changes

--- app/src/main/java/com/example/riddhipraise/cookingapp/signUp.java
+++ app/src/main/java/com/example/riddhipraise/cookingapp/signUp.java
@@ -1,13 +1,21 @@
 package com.example.riddhipraise.cookingapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class signUp extends AppCompatActivity {
+
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void goToMainPage(View view){
+        Intent intent = new Intent(this, MainPage.class);
+        startActivity(intent);
     }
 }
